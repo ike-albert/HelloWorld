ca6c2614bbdd09b71bd554fe27d580c5b429e016
ike-albert <ikeoluwa.albert@gmail.com>
2020-12-24 14:38:55 -0600

    Finished required features

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //User can tap on a button to change the text color of the label
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change the text color of the text
+                ((TextView) findViewById(R.id.text)).setTextColor(getResources().getColor(R.color.white));
+            }
+        });
+    }
+}
